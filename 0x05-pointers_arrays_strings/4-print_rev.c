c5b785871caec0391d82e3ec9cdc73e9a387a394
Nkemeni <nkemvale@gmail.com>
2023-03-27 15:42:25 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: string to be printed in reverse order
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i > -1)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+
+
+
+
