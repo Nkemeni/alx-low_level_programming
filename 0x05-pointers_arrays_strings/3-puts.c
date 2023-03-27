43c36bf38c15f4d36b9a8cf0049e75d822571562
Nkemeni <nkemvale@gmail.com>
2023-03-27 15:26:47 +0000

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a newline
+ * @str: string to be printed on the stdout
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
