0b93b5663eb9161d647969254dd6e35520c65d87
Nkemeni <nkemvale@gmail.com>
2023-04-12 21:42:59 +0000

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - determines the length of s string
+ * @s: the pointer to the string whose length is to be determine
+ *
+ * Return: the lenth of the string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+
+/**
+ * _strdup - a function that returns a pointer to a newly allocated space
+ * @str: pointer to string
+ *
+ * Return: NULL if str = NULL or On success
+ */
+char *_strdup(char *str)
+{
+	int len;
+	int i;
+	char *ptr;
+
+	len = _strlen(str);
+	ptr = malloc((len * sizeof(char)) + 1);
+
+	if (ptr != NULL)
+	{
+		for (i = 0; i < len; i++)
+		{
+			*(ptr + i) = *(str + i);
+		}
+		*(ptr + len) = *(str + len);
+	}
+
+	return (ptr);
+}
+
+
