208f4c14b3a1069334ba6b584f0a6d5f8637c646
Nkemeni <nkemvale@gmail.com>
2023-04-12 23:51:08 +0000

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = malloc(height * sizeof(int*));
+	ptr = malloc(sizeof(int *) * height);
 
 	if (ptr != NULL)
 	{
