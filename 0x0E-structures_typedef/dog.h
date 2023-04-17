3d79334c1a924a6185aa3580376559b445a98969
Nkemeni <nkemvale@gmail.com>
2023-04-17 12:20:32 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * struct dog - a structure that stores the name, age, and owner of a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+#endif
